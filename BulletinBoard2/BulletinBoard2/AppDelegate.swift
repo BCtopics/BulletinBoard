3c586896770c6e932569363b8cba219975d1f39a
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-25 10:09:37 -0600

    Clean up project

---
+++ BulletinBoard2/BulletinBoard2/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BulletinBoard2
+//
+//  Created by Bradley GIlmore on 4/25/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
