d42b5822621a75ffc7d0e8517ef5c3228bd3b466
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-25 10:32:24 -0600

    PostListTableViewController subclassed and setup in Main.storyboard

---
+++ BulletinBoard2/BulletinBoard2/PostListTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PostListTableViewController.swift
+//  BulletinBoard2
+//
+//  Created by Bradley GIlmore on 4/25/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class PostListTableViewController: UITableViewController {
+    
+    
+    
+}
